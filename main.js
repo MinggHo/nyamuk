3a6bb92eda6036e5ff8e4bef0bca694ef7dbc936
Mukmin Abd. Samad <mukmin.samad@revmedia.my>
2021-06-10 16:16:22 +0800

    social share using web-social-share library

--- main.js
+++ main.js
@@ -104,3 +104,65 @@ const runningAnimation = new IntersectionObserver((entries) => {
 }, { threshold: 0.25 });
 
 runningAnimation.observe(contentRunning);
+
+
+function show(showValue) {
+  const elem = document.getElementsByTagName('web-social-share');
+  if (elem && elem.length > 0) {
+    elem[0].show = showValue;
+  }
+}
+
+// Share options
+const articleLink = 'https://exclusive.says.com/my/exclusive/why-mosquitoes-bite-you/';
+const share = {
+  displayNames: true,
+  config: [
+    {
+      facebook: {
+        socialShareUrl: articleLink,
+        socialSharePopupWidth: 400,
+        socialSharePopupHeight: 400
+      }
+    },
+    {
+      twitter: {
+        socialShareUrl: articleLink,
+        socialSharePopupWidth: 300,
+        socialSharePopupHeight: 400
+      }
+    },
+    {
+      whatsapp: {
+        socialShareText: `Why Do Some People Always Get Bitten By Mosquitoes While Others Don't?`,
+        socialShareUrl: articleLink
+      }
+    },
+    {
+      copy: {
+        socialShareUrl: articleLink
+      }
+    },
+    {
+      telegram: {
+        socialShareText: `Why Do Some People Always Get Bitten By Mosquitoes While Others Don't?`,
+        socialShareUrl: articleLink,
+        socialSharePopupWidth: 300,
+        socialSharePopupHeight: 400
+      }
+    }
+  ]
+};
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const elem = document.getElementsByTagName('web-social-share');
+  if (elem && elem.length > 0) {
+    elem[0].share = share;
+  }
+});
+
+const openSocialShareButton = document.querySelector("#social-share");
+
+openSocialShareButton.onclick = () => {
+  show(true);
+}
